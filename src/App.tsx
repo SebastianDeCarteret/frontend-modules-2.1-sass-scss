ba3634123d783bfe89e07f2b4f5df6a9f6386ff3
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2024-10-11 16:20:01 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./main.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="big big-pink">Hello, world</h1>
+      <h2>Stay awhile, and listen!</h2>
+      <button className="btn btn-primary">Click me!</button>
+    </div>
+  );
+}
+
+export default App;
